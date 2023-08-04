d633c116b00dec3e57057458e993cf9c12dd78a7
Harry Bragg <harry.bragg@poweredbypercent.com>
2023-08-04 15:18:04 +0100

    define pieces happy places

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,32 +2,59 @@
 using System;
 using System.Linq;
 
+delegate int PieceLocationModifier(Square square);
+
 public class MyBot : IChessBot
 {
-    readonly int[] pieceValues = { 100, 300, 300, 500, 900, 10000 };
+    // null, pawn, knight, bishop, rook, queen, king
+    readonly int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    readonly PieceLocationModifier[] modifiers = {
+        // null
+        s => 0,
+        // pawn
+        s => s.Rank > 3 ? 30 : (s.Rank == 2 && s.File > 2 && s.File < 5 ? 20 : 0),
+        // knight
+        s => s.Rank == 0 || s.Rank == 7 || s.File == 0 || s.File == 7 ? -40 : (4 - Math.Abs(s.Rank - 4) + (4 - Math.Abs(s.Index - 4)))*10,
+        // bishop
+        s => s.Rank == 0 || s.Rank == 7 || s.File == 0 || s.File == 7 ? -10 : 1,
+        // rook
+        s => s.Rank == 0 || s.File == 0 || s.File == 7 ? -10 : s.File == 6 ? 10 : 0,
+        // queen
+        s => s.Rank == 0 || s.Rank == 7 || s.File == 0 || s.File == 7 ? -10 : 0,
+        // king
+        s => s.Rank > 2 ? -20 : s.Index < 3 || s.Index > 5 ? 20 : 0,
+    };
 
     public Move Think(Board board, Timer timer)
     {
-        (Move?, int) next = Search(board, 3, int.MinValue, int.MaxValue, true, board.IsWhiteToMove);
-        if (next.Item1 != null) {
+        (Move?, int) next = Search(board, 5, int.MinValue, int.MaxValue, true, board.IsWhiteToMove);
+        if (next.Item1 != null)
+        {
             return (Move)next.Item1;
         }
         return board.GetLegalMoves()[0];
     }
 
-    private int Score(Board board, bool white) {
+    private int Score(Board board, bool white)
+    {
         if (board.IsInCheckmate()) return 1000000;
+        if (board.IsDraw()) return 0;
         int pieceScore = board.GetAllPieceLists()
-            .Sum(list => pieceValues[(int)list.TypeOfPieceInList - 1] * list.Count * (list.IsWhitePieceList == white ? 1 : -1));
-        return pieceScore;
+            .Sum(list => ((pieceValues[(int)list.TypeOfPieceInList] * list.Count) +
+                list.Sum(piece => modifiers[(int)piece.PieceType](piece.Square))) * (list.IsWhitePieceList == white ? 1 : -1)
+            );
+
+        int inCheckMod = board.IsInCheck() ? 400 : 0;
+        return pieceScore + inCheckMod;
     }
 
-    private (Move?, int) Search(Board board, int depth, int alpha, int beta, bool isMax, bool white) {
+    private (Move?, int) Search(Board board, int depth, int alpha, int beta, bool isMax, bool white)
+    {
         if (depth == 0) { return (null, Score(board, white)); }
         Random random = new();
         // randomise order
         Move[] moves = board.GetLegalMoves()
-                .OrderByDescending(move => move.IsCapture ? pieceValues[(int)move.CapturePieceType - 1] : random.Next(50))
+                .OrderByDescending(move => move.IsCapture ? pieceValues[(int)move.CapturePieceType] : random.Next(5))
                 .ToArray();
         if (moves.Length == 0) { return (null, Score(board, white)); }
         Move? bestMove = null;
@@ -35,27 +62,28 @@ public class MyBot : IChessBot
         int value = isMax ? int.MinValue : int.MaxValue;
         bool better(int score) { return isMax ? score > value : score < value; }
 
-        foreach (Move move in moves) {
+        foreach (Move move in moves)
+        {
             board.MakeMove(move);
             (Move?, int) next = Search(board, depth - 1, alpha, beta, !isMax, white);
-            if (better(next.Item2)) {
+            bool doBreak = false;
+            if (better(next.Item2))
+            {
                 value = next.Item2;
                 bestMove = move;
-                if (isMax) {
+                if (isMax)
+                {
+                    doBreak = value >= beta;
                     alpha = Math.Max(alpha, value);
-                    if (value >= beta) {
-                        board.UndoMove(move);
-                        break;
-                    }
-                } else {
+                }
+                else
+                {
+                    doBreak = value <= alpha;
                     beta = Math.Min(beta, value);
-                    if (value <= alpha) {
-                        board.UndoMove(move);
-                        break;
-                    }
                 }
             }
             board.UndoMove(move);
+            if (doBreak) { break; }
         }
         return (bestMove, value);
     }
