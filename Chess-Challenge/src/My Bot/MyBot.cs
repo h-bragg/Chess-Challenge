6a946e9037bb6223d0189438e5bd5a941b741016
Harry Bragg <harry.bragg@poweredbypercent.com>
2023-08-04 12:45:16 +0100

    alphabeta version

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,60 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
+    readonly int[] pieceValues = { 10, 30, 30, 50, 90, 1000 };
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        (Move?, int) next = Search(board, 3, int.MinValue, int.MaxValue, true, board.IsWhiteToMove);
+        if (next.Item1 != null) {
+            return (Move)next.Item1;
+        }
+        return board.GetLegalMoves()[0];
+    }
+
+    private int Score(Board board, bool white) {
+        if (board.IsInCheckmate()) return 1000000;
+        int pieceScore = board.GetAllPieceLists()
+            .Sum(list => pieceValues[(int)list.TypeOfPieceInList - 1] * list.Count * (list.IsWhitePieceList == white ? 1 : -1));
+        return pieceScore;
+    }
+
+    private (Move?, int) Search(Board board, int depth, int alpha, int beta, bool isMax, bool white) {
+        if (depth == 0) { return (null, Score(board, white)); }
+        Move[] moves = board.GetLegalMoves()
+                .OrderByDescending(move => move.IsCapture ? pieceValues[(int)move.CapturePieceType - 1] : 1)
+                .ToArray();
+        if (moves.Length == 0) { return (null, Score(board, white)); }
+        Move? bestMove = null;
+
+        int value = isMax ? int.MinValue : int.MaxValue;
+        bool better(int score) { return isMax ? score > value : score < value; }
+
+        foreach (Move move in moves) {
+            board.MakeMove(move);
+            (Move?, int) next = Search(board, depth - 1, alpha, beta, !isMax, white);
+            if (better(next.Item2)) {
+                value = next.Item2;
+                bestMove = move;
+                if (isMax) {
+                    alpha = Math.Max(alpha, value);
+                    if (value >= beta) {
+                        board.UndoMove(move);
+                        break;
+                    }
+                } else {
+                    beta = Math.Max(beta, value);
+                    if (value >= alpha) {
+                        board.UndoMove(move);
+                        break;
+                    }
+                }
+            }
+            board.UndoMove(move);
+        }
+        return (bestMove, value);
     }
 }
